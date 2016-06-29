734e0ea64c89f622c8dd8a8c9bd984182b1e3f61
vives <vsvivevives@gmail.com>
2016-06-29 18:21:26 +0530

    Update ZohorecruitConnectorIntegrationTest.java

--- src/test/java/org/wso2/carbon/connector/integration/test/zohorecruit/ZohorecruitConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/zohorecruit/ZohorecruitConnectorIntegrationTest.java
@@ -43,7 +43,7 @@ public class ZohorecruitConnectorIntegrationTest extends ConnectorIntegrationTes
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
 
-        init("zohorecruit-connector-1.0.1-SNAPSHOT");
+        init("zohorecruit-connector-1.0.1");
 
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/json");
